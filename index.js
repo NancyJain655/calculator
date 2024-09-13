924b57a42e579b3aad223fda8ad17f42af866761
NancyJain655 <jainnancy655@gmail.com>
2024-09-13 08:23:18 -0700

    commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const display=document.getElementById("display");
+function appendToDisplay(input)
+{if (display.value[0]=='+'||display.value[0]=='/'||display.value[0]=='*')
+{
+    display.value="invalid";
+}
+else{
+    display.value += input;
+}
+}
+function clearDisplay()
+{
+    display.value="";
+}
+function deleteDisplay(){
+display.value=display.value.slice(0,-1);
+}
+function calculate()
+{ 
+    try{
+if(Number.isInteger(eval(display.value)))
+    {
+      display.value=eval(display.value);
+    }
+    else{
+        display.value=eval(display.value).toFixed(3);
+    }
+ 
+    }
+    
+    catch(error)
+{
+    display.value="invalid";
+}
+ 
+}
